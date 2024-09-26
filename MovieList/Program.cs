41ba395cb30c9abe950b1cbef768c07c051bb15e
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-09-26 09:35:48 -0500

    Add step 12.

--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MovieContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MovieContext")));
 
 var app = builder.Build();
 
@@ -17,8 +24,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
